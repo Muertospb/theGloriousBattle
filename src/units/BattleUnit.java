d944dbea66a939400034aa8ced3a10a83c0c2b72
Muertospb <8muerto8@gmail.com>
2019-11-02 13:19:04 +0300

    HomeWork 02/11/19

--- src/units/BattleUnit.java
+++ src/units/BattleUnit.java
@@ -1,6 +1,8 @@
 package units;
 
-abstract public class BattleUnit extends Unit implements Attackable{
+import java.util.Random;
+
+abstract public class BattleUnit extends Unit implements Attackable {
 
     protected int attackScore;
 
@@ -15,13 +17,12 @@ abstract public class BattleUnit extends Unit implements Attackable{
     }
 
     public void setAttackScore(int attackScore) {
-       if (attackScore > 0) {
-           this.attackScore = attackScore;
-       } else {
-           this.attackScore = 1;
-       }
+        if (attackScore > 0) {
+            this.attackScore = attackScore;
+        } else {
+            this.attackScore = 1;
+        }
     }
-
 
 
     @Override
@@ -38,4 +39,44 @@ abstract public class BattleUnit extends Unit implements Attackable{
     public void escapeBattleField() {
         System.out.println("Юнит " + getName() + " сбежал с поля боя");
     }
+
+/*В абстрактном классе BattleUnit создать статический метод, возвращающий объекты боевых юнитов, в зависимости от
+переданного типа юнита ("warrior", "knight", "doctor"). Характеристики задавать рандомно, имя задавать следующим
+образом: тип юнита + рандомное число.*/
+
+    public static BattleUnit getUnit(String type, int speed, int health, int attackScore){
+        Random random = new Random();
+        BattleUnit unit = null;
+
+        if (speed < 1 || 10 < speed ){
+            speed = random.nextInt(10) + 1;
+        }
+
+        if (health < 10 || Unit.maxHealth < health){
+            //Если переданное здоровье меньше 10 или больше максимального, то устанавливается рандом 10-100
+            health = random.nextInt(Unit.maxHealth - 10) + 10;
+        }
+
+        if (attackScore < 1 || 20 < attackScore){
+            attackScore = random.nextInt(20) + 1;
+        }
+
+        if (type.equals("warrior")){
+            unit = new Warrior("Warrior" + random.nextInt(99999), speed, health, attackScore);
+        }
+        if (type.equals("knight")){
+            unit = new Knight("Knight" + random.nextInt(99999), speed, health, attackScore);
+        }
+        if (type.equals("doctor")){
+            unit = new Doctor("Doctor" + random.nextInt(99999), speed, health, attackScore);
+        }
+
+        return unit;
+    }
+
+//Перегруженный метод для генерации объекта только по типу
+    public static BattleUnit getUnit(String type){
+       return getUnit(type, 0, 0, 0);
+    }
+
 }
--- src/units/King.java
+++ src/units/King.java
@@ -1,23 +1,41 @@
 package units;
 
-public class King extends Unit {
-    public King(String name, int speed, int health) {
-        super(name, speed);
-        setInitHealth(health);
+
+
+
+public class King implements Restable{
+
+    private String name;
+    private int gold;
+
+    // модификатор с private изменять нельзя!!!
+    // другие конструкторы создавать нельзя!!!
+    private King(String name, int gold) {
+        this.name = name;
+        this.gold = gold;
     }
+
+    public static King getKing (String name, int gold){
+        return new King(name, gold);
+    }
+
+
 
     @Override
     public String toString() {
         return "King{" +
                 "name='" + name + '\'' +
-                ", health=" + health +
-                ", speed=" + speed +
+                ", gold=" + gold +
                 '}';
     }
 
+
+
+
     @Override
     public void rest() {
-        System.out.println("Король отдыхает, что ему еще делать? Восстановил " + (maxHealth + getSpeed()) / 10 + " здоровья");
-        setHealth(getHealth() + (maxHealth + getSpeed()) / 10);
+        System.out.println("Король отдыхает");
     }
 }
+
+
